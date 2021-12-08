3a4014ef0f95499942801e856e77fed65f01e75c
Jonathan Stichbury <2533428+nzjony@users.noreply.github.com>
2021-12-08 16:04:18 +0100

    try recursion in web-worker thread
    Signed-off-by: Jonathan Stichbury <2533428+nzjony@users.noreply.github.com>

--- worker.js
+++ worker.js
@@ -1,31 +1,17 @@
-onmessage = function(e){
-  if ( e.data === "start" ) {
+var i = 0;
+function recurse() {
+  i++;
+  try {
+    console.count("here in the ww");
+    recurse();
+  } catch (e) {
+    console.log("stopped at iteration " + i);
+  }
+}
 
-		var a = [];
-
-		for (var i = 50000; i >= 0; i--) {
-		    a.push(i);
-		};
-
-		function bubbleSort(a)
-		{
-		    var swapped;
-		    do {
-		        swapped = false;
-		        for (var i=0; i < a.length-1; i++) {
-		            if (a[i] > a[i+1]) {
-		                var temp = a[i];
-		                a[i] = a[i+1];
-		                a[i+1] = temp;
-		                swapped = true;
-		            }
-		        }
-		    } while (swapped);
-		}
-		var start = new Date().getTime();
-		bubbleSort(a);
-		var end = new Date().getTime();
-		var time = end - start;
-		postMessage(time);
+onmessage = function (e) {
+  if (e.data === "start") {
+    console.log("starting");
+    recurse();
   }
 };
